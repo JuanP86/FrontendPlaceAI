15089b2ad1b5b405e7b75f68222280e154fd85ea
unknown <juanpablopatxot2003@gmail.com>
2021-06-04 12:11:35 -0300

    cambiando tabla

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { useTable } from 'react-table';
-
+import { useTable, useFilters, useGlobalFilter, useAsyncDebounce } from 'react-table'
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -30,7 +29,7 @@ export default function App() {
               <div className="collapse navbar-collapse" id="navbarToggler">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <Link to="/inspeccionar" className="nav-link">About</Link>
+                    <Link to="/inspeccionar" className="nav-link">Inspeccionar</Link>
                   </li>
                   <li></li>
                 </ul>
@@ -64,28 +63,60 @@ function Inspeccionar() {
   const data = React.useMemo(
     () => [
       {
-        col1: "Magic Kingdom",
+        col1: "E.T Adventure",
         col2: "40 minutos",
       },
       {
-        col1: "Universal Studios",
+        col1: "Hollywood Rip Ride Rockit",
         col2: "20 minutos",
       },
       {
-        col1: "SeaWorld",
+        col1: "Kang & Kodos' Twirl 'n' Hurl",
         col2: "30 minutos",
-      }
+      },
+      {
+        col1: "Revenge of the Mummy",
+        col2: "30 minutos",
+      },
+      {
+        col1: "Shrek 4-D",
+        col2: "30 minutos",
+      },
+      {
+        col1: "MEN IN BLACK™ Alien Attack™",
+        col2: "20 minutos",
+      },
+      {
+        col1: "Monsters, Inc. Laugh Floor",
+        col2: "5 minutos",
+      },
+      {
+        col1: "Walt Disney World Railroad - Frontierland",
+        col2: "10 minutos",
+      },
+      {
+        col1: "Walt Disney's Enchanted Tiki Room",
+        col2: "40 minutos",
+      },
+      {
+        col1: "Sorcerers of the Magic Kingdom",
+        col2: "15 minutos",
+      },
+      {
+        col1: "The Barnstormer",
+        col2: "25 minutos",
+      },
     ], []
   );
 
   const columns = React.useMemo(
     () => [
       {
-        Header: "Column 1",
+        Header: "Juegos",
         accessor: "col1",
       },
       {
-        Header: "Column 2",
+        Header: "Tiempos de Espera",
         accessor: "col2",
       }
     ], []
@@ -101,51 +132,52 @@ function Inspeccionar() {
 
   return (
     <div className="container-fluid" id="Inspeccionar-Div">
-    <table {...getTableProps()} style={{ border: 'solid 1px white' }}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  borderBottom: 'solid 3px gray',
-                  background: 'black',
-                  color: 'white',
-                  fontWeight: 'bold',
-                }}
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      padding: '10px',
-                      border: 'solid 1px gray',
-                      background: 'black',
-                      color: "white",
-                    }}
-                  >
-                    {cell.render('Cell')}
-                  </td>
-                )
-              })}
+      <table {...getTableProps()} style={{ border: 'solid 1px white' }}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    borderBottom: 'solid 3px gray',
+                    background: 'black',
+                    color: 'white',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: '10px',
+                        border: 'solid 1px gray',
+                        background: 'black',
+                        color: "white",
+                        width: "250px",
+                      }}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
